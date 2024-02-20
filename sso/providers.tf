178582f8fe44452a2270b40b07a9ab50c91c5e8e
James Bernardi <jamesbernardi@users.noreply.github.com>
2024-02-20 14:14:01 -0500

    provider update

--- sso/providers.tf
+++ sso/providers.tf
@@ -6,7 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.31"
 
-      configuration_aliases = [aws.target]
+      configuration_aliases = [
+        aws.target,
+        aws.main
+      ]
     }
   }
 }
