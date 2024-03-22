d280bec66af2a6e8e573d8a854fc0b61036b1fc9
James Bernardi <jamesbernardi@users.noreply.github.com>
2024-03-22 11:54:53 -0400

    adding vpc outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -99,3 +99,18 @@ output "cluster_roles" {
     ])
   )
 }
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "availability_zones" {
+  description = "List of availability zones"
+  value       = module.vpc.azs
+}
