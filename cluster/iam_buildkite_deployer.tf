46b22f0f1c26ae94a127302682518b396d663f5f
gustavderdrache <aford@forumone.com>
2024-07-30 12:57:52 -0400

    Fix attribute in caller_identity

--- cluster/iam_buildkite_deployer.tf
+++ cluster/iam_buildkite_deployer.tf
@@ -68,8 +68,8 @@ data "aws_iam_policy_document" "buildkite_deploy_ecs" {
 
     # Scope tagging to task definitions and services only
     resources = [
-      "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current}:service/${var.name}/*",
-      "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current}:task-definition:*",
+      "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:service/${var.name}/*",
+      "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:task-definition:*",
     ]
   }
 
