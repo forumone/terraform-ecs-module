cfb6c3c852e6eea6de1b720ff2563a01e0edbf11
Jo Ryden <jryden@f1c.io>
2025-06-24 22:03:42 +0200

    Up EBS size

--- cluster/auto_files_export.tf
+++ cluster/auto_files_export.tf
@@ -59,7 +59,7 @@ resource "aws_ssm_document" "files_export" {
               DeviceName = "/dev/xvda"
 
               Ebs = {
-                VolumeSize = 4096
+                VolumeSize = 16384
               }
             }
           ]
